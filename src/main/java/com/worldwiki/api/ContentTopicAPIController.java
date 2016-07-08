1e4d2c8a27fdd96c2cbe6a47093adb3dd4435a17
Matthew Remmel <matt.remmel@gmail.com>
2016-07-07 21:43:45 -0400

    Moved ContentTopicAPIController into 'api' package

--- src/main/java/com/worldwiki/api/ContentTopicAPIController.java
+++ src/main/java/com/worldwiki/api/ContentTopicAPIController.java
@@ -5,7 +5,7 @@
 // Created by Matthew Remmel on 7/5/16.
 // Copyright (c) 2016 Matthew Remmel. All rights reserved.
 
-package com.worldwiki.controller;
+package com.worldwiki.api;
 
 import com.worldwiki.exception.ContentTopicNotFoundException;
 import com.worldwiki.model.world.topic.ContentTopic;
