ccd80f4022937787d5ef8518db9ee53175ab9edd
Matthew Remmel <matt.remmel@gmail.com>
2016-07-08 01:00:04 -0400

    Added more functionality for ContentTopic view and editing page

--- src/main/resources/public/js/ContentTopicControllers.js
+++ src/main/resources/public/js/ContentTopicControllers.js
@@ -14,4 +14,33 @@ angular.module('ContentTopic.controllers').controller('ContentTopicController', 
     }, function failed(response) {
         console.log('An error occurred: ' + response.statusText);
     });
+
+    $scope.startEditMode = function() {
+        $scope.editMode = true;
+    };
+
+    $scope.cancelEditMode = function() {
+        $scope.editMode = false;
+    };
+
+    $scope.addTextSnippet = function(index) {
+        $scope.topicData.snippetGroups[index].snippets.push({
+            "type": "TextSnippet",
+            "title": "Title",
+            "annotations": [],
+            "truthLevel": "Truth",
+            "revealed": false,
+            "style": "Normal",
+            "content": ""
+        });
+    };
+
+    $scope.deleteSnippet = function(group, index) {
+        $scope.topicData.snippetGroups[group].snippets.splice(index, 1);
+    };
+
+    $scope.saveContentTopic = function() {
+        console.log("Saving content topic");
+        $scope.editMode = false;
+    };
 });
